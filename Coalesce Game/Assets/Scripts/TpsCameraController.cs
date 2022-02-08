aacbe823ce3f93253a0f4c8e8a1ff68fdcd8f5eb
Guanomancer <78472401+Guanomancer@users.noreply.github.com>
2022-02-08 11:54:47 +0100

    Implemented basic character script

--- Coalesce Game/Assets/Scripts/TpsCameraController.cs
+++ Coalesce Game/Assets/Scripts/TpsCameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Coalesce
 {
@@ -8,14 +9,34 @@ namespace Coalesce
     {
         [SerializeField]
         private Transform _cameraBoom;
+        [SerializeField, Range(10, 80)]
+        private float _cameraXClampAngle = 25f;
+
+        private float _rotationOffsetX;
+        private float _rotationX;
 
         private void Awake()
         {
-            if(_cameraBoom == null)
+            if (_cameraBoom == null)
             {
                 Debug.LogWarning($"No camera boom set for {nameof(TpsCameraController)}. Disabling.", this);
                 enabled = false;
+                return;
             }
+
+            _rotationOffsetX = _cameraBoom.localEulerAngles.x;
+        }
+
+        public void CameraInput(InputAction.CallbackContext context)
+        {
+            var vector = context.ReadValue<Vector2>();
+            Debug.Log(vector);
+
+            _rotationX = Mathf.Clamp(_rotationX + vector.y, -_cameraXClampAngle, _cameraXClampAngle);
+            var eulers = _cameraBoom.localEulerAngles;
+            eulers.x = _rotationOffsetX + _rotationX;
+            eulers.y += vector.x;
+            _cameraBoom.localEulerAngles = eulers;
         }
     }
 }
