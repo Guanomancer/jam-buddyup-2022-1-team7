8e63c32872746693b98f6ca646ae4ee866c878e9
Guanomancer <78472401+Guanomancer@users.noreply.github.com>
2022-02-09 23:22:40 +0100

    Implemented animations and Sound for the toddler!

--- Coalesce Game/Assets/Scripts/TodzillaController.cs
+++ Coalesce Game/Assets/Scripts/TodzillaController.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 namespace Coalesce
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class TodzillaController : MonoBehaviour
+    [RequireComponent(typeof(AudioSource))]
+    public class TodzillaController : MonoBehaviour, IEventSubscriber
     {
         [SerializeField, Tooltip("Max speed in m/s")]
         private float _crawlSpeed = 1f;
@@ -18,8 +19,18 @@ namespace Coalesce
         private Transform _cameraBoom;
         [SerializeField]
         private Transform _model;
+        [SerializeField, Range(0.01f, 1f)]
+        private float _animatorVelocityWalkThreshold = 0.1f;
+
+        [Header("Audio")]
+        [SerializeField]
+        private AudioClip _leftFootstep;
+        [SerializeField]
+        private AudioClip _rightFootstep;
 
         private Rigidbody _body;
+        private Animator _animator;
+        private AudioSource _audio;
 
         private Vector3 _crawlInput = Vector3.zero;
         private Vector3 _velocity = Vector3.zero;
@@ -29,13 +40,50 @@ namespace Coalesce
             => _crawlInput = context.ReadValue<Vector2>().RemapToVector3();
 
         private void Awake()
-            => _body = GetComponent<Rigidbody>();
+        {
+            _body = GetComponent<Rigidbody>();
+            _animator = GetComponentInChildren<Animator>();
+            if (_animator == null)
+                Debug.LogWarning("No animator found in GameObject or any of the children.", this);
+            _audio = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            EventRouter.Subscribe(EventName.TodzillaRightFoot, this);
+            EventRouter.Subscribe(EventName.TodzillaLeftFoot, this);
+        }
+
+        private void OnDisable()
+        {
+            EventRouter.Unsubscribe(EventName.TodzillaRightFoot, this);
+            EventRouter.Unsubscribe(EventName.TodzillaLeftFoot, this);
+        }
+
+        public void OnEvent(EventName eventName, IEventDispatcher dispatcher)
+        {
+            switch(eventName)
+            {
+                case EventName.TodzillaRightFoot:
+                    _audio.PlayOneShot(_rightFootstep);
+                    break;
+                case EventName.TodzillaLeftFoot:
+                    _audio.PlayOneShot(_leftFootstep);
+                    break;
+            }
+        }
 
         private void FixedUpdate()
         {
             ApplyMovement();
             ApplyRotation();
             ApplyVelocity();
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            _animator.SetBool("IsWalking", _velocity.sqrMagnitude >= _animatorVelocityWalkThreshold);
         }
 
         private void ApplyRotation()
