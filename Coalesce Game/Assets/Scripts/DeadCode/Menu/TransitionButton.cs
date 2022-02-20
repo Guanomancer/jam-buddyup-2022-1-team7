1e3ce02fca9296271b1759fb840fbb3e7beb0994
Guanomancer <78472401+Guanomancer@users.noreply.github.com>
2022-02-20 20:51:00 +0100

    Build #20.3

--- Coalesce Game/Assets/Scripts/DeadCode/Menu/TransitionButton.cs
+++ Coalesce Game/Assets/Scripts/DeadCode/Menu/TransitionButton.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Coalesce.DeadCode;
+using UnityEngine.SceneManagement;
 
 namespace Coalesce
 {
     public class TransitionButton : MonoBehaviour
     {
         public void Transition(int sceneIndex)
-            => GameManager.Instance.TransitionScene(sceneIndex);
+            => SceneManager.LoadScene(sceneIndex);
     }
 }
